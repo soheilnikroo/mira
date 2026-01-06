638f4e73e0de60f541d5ca329c2177e4374d5c1b
Soheil Nikroo <soheiln1234@gmail.com>
2026-01-06 19:35:33 +0330

    feat: add Radix UI components and update dashboard layout
    - Introduced new Navbar component for enhanced navigation in the dashboard.
    - Added Dialog and Tooltip components using Radix UI for improved UI interactions.
    - Updated dashboard layout to include the new Navbar component.
    - Added new dependencies for Radix UI in package.json and pnpm-lock.yaml.

---
+++ app/(dashboard)/_components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import { UserButton } from '@clerk/nextjs';
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg:flex-1 lg:flex">{/* add search */}</div>
+      <UserButton />
+    </div>
+  );
+};
+
+export default Navbar;
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { Navbar } from './_components/navbar';
 import { OrganizationSidebar } from './_components/organization-sidebar';
 import { Sidebar } from './_components/sidebar';
 
@@ -13,7 +14,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
         <div className="flex gap-x-3 h-full">
           <OrganizationSidebar />
           <div className="h-full flex-1">
-            {/* Add Navbar */}
+            <Navbar />
             {children}
           </div>
         </div>
